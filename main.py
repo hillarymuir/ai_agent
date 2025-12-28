0b45b06b0a503d616787ad5839d406ec5f53feec
hillarymuir <hillarymuir@pm.me>
2025-12-28 11:25:01 -0500

    fix: .env file name fixed so load_dotenv() sees it and to match convention

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+# imports
 import os
 from dotenv import load_dotenv
+from google import genai
 
 def main():
     # get api key
     load_dotenv()
+
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
         raise RuntimeError("No API key found.")
