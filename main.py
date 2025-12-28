233b21bf9db8b60ca73ab958e3bf7eb317c1a51a
hillarymuir <hillarymuir@pm.me>
2025-12-28 10:59:45 -0500

    start: set up project (venv, git, api key, etc)

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+def main():
+    # get api key
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("No API key found.")
+    
+    # create Gemini client
+
+    print("Hello from ai-agent!")
+
+
+if __name__ == "__main__":
+    main()
